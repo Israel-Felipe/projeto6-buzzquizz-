e78d2db41d6b6daa024542375cb1e6e3595a684f
Israel-Felipe <israelfelipet@gmail.com>
2022-06-30 22:26:33 -0300

    edicao de perguntas funcional - tela 2 criacao do quizz

--- script.js
+++ script.js
@@ -27,23 +27,23 @@ function pegarValues1 () {
     tituloQuizz = document.querySelector(".criacaoConteudo div:nth-child(1) input").value;
     imgQuizz = document.querySelector(".criacaoConteudo div:nth-child(2) input").value;
     qtdPerg = document.querySelector(".criacaoConteudo div:nth-child(3) input").value;
-    qtdniveis = document.querySelector(".criacaoConteudo div:nth-child(4) input").value;
+    qtdNiveis = document.querySelector(".criacaoConteudo div:nth-child(4) input").value;
 
 
     if ((tituloQuizz === null) || (tituloQuizz.length < 20) || (tituloQuizz.length > 65) ||
         (imgQuizz[0] !== "h" && imgQuizz[1] !== "t" && imgQuizz[2] !== "t" && imgQuizz[3] !== "p") ||
-        (qtdPerg < 3) ||
-        (qtdNiveis < 2)) {
-        alert("Escreve certinho aí pf");
+        (Number(qtdPerg) < 3) ||
+        (Number(qtdNiveis) < 2)) {
+        /* alert("Escreve certinho aí pf"); */
+        criarQuizz2(); /* TIRAR ISSO AQUI DEPOIS! É só pra poder testar mais rapido */
     } 
-    
-    
-    
     else {
         criarQuizz2();
     }
 }
 
+let arrayPerguntasAbertas = [];
+let arrayPerguntasFechadas = [];
 
 function criarQuizz2() {
     container.innerHTML = `
@@ -51,41 +51,102 @@ function criarQuizz2() {
         <div class="criacaoTitulo">
             <h1>Crie suas perguntas</h1>
         </div>
-
-        <div class="criacaoConteudo">
-            <h2>Pergunta 1</h2>
-            <div class="box-input" ><input placeholder="Texto da pergunta"></div>
-            <div class="box-input"><input placeholder="Cor de fundo da pergunta"></div>
-
-            <h2>Resposta correta</h2>
-            <div class="box-input"><input placeholder="Resposta correta"></div>
-            <div class="box-input"><input placeholder="URL da imagem"></div>
-
-            <h2>Respostas incorretas</h2>
-            <div class="box-input"><input placeholder="Resposta incorreta 1"></div>
-            <div class="box-input"><input placeholder="URL da imagem 1"></div>
-            <br>
-            <div class="box-input"><input placeholder="Resposta incorreta 2"></div>
-            <div class="box-input"><input placeholder="URL da imagem 2"></div>
-            <br>
-            <div class="box-input"><input placeholder="Resposta incorreta 3"></div>
-            <div class="box-input"><input placeholder="URL da imagem 3"></div>
-        </div>
-
-        <div class="criacaoConteudoMini">
-            <h2>Pergunta 2</h2>
-            <img src="img/editar.svg" alt="">
-        </div>
-
-        <div class="criacaoConteudoMini">
-            <h2>Pergunta 3</h2>
-            <img src="img/editar.svg" alt="">
-        </div>
-
-        <div class="botaoVermelho370" onclick="criarQuizz3()">Prosseguir pra criar níveis</div>
     </div>
     `
+        for (let i=0; i<qtdPerg; i++) {
+        let perguntaModelo = `
+        <div class="perguntaAberta escondido">
+            <div class="criacaoConteudo">
+            <h2>Pergunta ${i+1}</h2>
+                <div class="box-input" ><input placeholder="Texto da pergunta"></div> 
+                <div class="box-input"><input placeholder="Cor de fundo da pergunta"></div>
+
+                <h2>Resposta correta</h2>
+                <div class="box-input"><input placeholder="Resposta correta"></div>
+                <div class="box-input"><input placeholder="URL da imagem"></div>
+
+                <h2>Respostas incorretas</h2>
+                <div class="box-input"><input placeholder="Resposta incorreta 1"></div>
+                <div class="box-input"><input placeholder="URL da imagem 1"></div>
+                <br>
+                <div class="box-input"><input placeholder="Resposta incorreta 2"></div>
+                <div class="box-input"><input placeholder="URL da imagem 2"></div>
+                <br>
+                <div class="box-input"><input placeholder="Resposta incorreta 3"></div>
+                <div class="box-input"><input placeholder="URL da imagem 3"></div>
+            </div>
+        </div>`
+        let perguntaModeloFechado = `
+        <div class="perguntaFechada">
+            <div class="criacaoConteudoMini">
+                <h2>Pergunta ${i+1}</h2>
+                <img src="img/editar.svg" onclick="editarPergunta(this)">
+            </div>
+        </div>`
+
+        arrayPerguntasAbertas.push(`${perguntaModelo}`);
+        arrayPerguntasFechadas.push(`${perguntaModeloFechado}`);
+        
+        container.innerHTML += arrayPerguntasAbertas[i];
+        container.innerHTML += arrayPerguntasFechadas[i];
+        }
+
+        document.querySelector(".perguntaAberta").classList.remove("escondido");
+        document.querySelector(".perguntaFechada").classList.add("escondido");
+
+        container.innerHTML += `<div class="botaoVermelho370" onclick="criarQuizz3()">Prosseguir pra criar níveis</div>`
 }
+
+
+function editarPergunta(elemento) {
+    let perguntasAbertas = document.querySelectorAll(".perguntaAberta");
+
+    for (let i=0; i<perguntasAbertas.length; i++) {
+        perguntasAbertas[i].classList.add("escondido");
+    }
+        
+
+    let perguntasFechadas = container.querySelectorAll(".perguntaFechada");
+    for (let i=0; i<perguntasFechadas.length; i++) {
+        perguntasFechadas[i].classList.remove("escondido");
+    }
+
+   
+
+    let elementoPai = elemento.parentNode;
+    let elementoAvo = elementoPai.parentNode;
+
+
+    let elementoAnterior = elementoAvo.previousElementSibling;
+
+    elementoAvo.classList.add("escondido");
+
+    elementoAnterior.classList.remove("escondido");
+
+}
+
+
+
+
+
+
+/* function pegarValues2 () {
+
+
+     = document.querySelector(".criacaoConteudo div:nth-child(1) input").value;
+     = document.querySelector(".criacaoConteudo div:nth-child(2) input").value;
+     = document.querySelector(".criacaoConteudo div:nth-child(3) input").value;
+     = document.querySelector(".criacaoConteudo div:nth-child(4) input").value;
+
+
+    if () {
+        alert("Escreve certinho aí pf");
+    } 
+    else {
+        criarQuizz3();
+    }
+}
+ */
 
 function criarQuizz3() {
     container.innerHTML = `
@@ -206,7 +267,7 @@ function EnviarQuizz() {
 }
 
 function quizzEnviado() {
-    console.log("Login efetuado!")
+    console.log("Quizz enviado com sucesso!")
     criarQuizz4();
  }
  function erroCriacaoQuizz(erro) {
