184e08bc86fb1213813921093c929a96ef408557
Israel-Felipe <israelfelipet@gmail.com>
2022-07-03 12:22:22 -0300

    criacao quizz desktop concluida

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 container = document.querySelector(".container");
 
-
+/* ------------------------------- INICIA TELA 1 DO QUIZZ - INFORMAÇÕES BÁSICAS */
 function criarQuizz1() {
     container.innerHTML = `
     <div class="criacaoQuizz">
@@ -34,14 +34,14 @@ function pegarValues1 () {
         (imgQuizz.substring(0,4) !== "http") ||
         (Number(qtdPerg) < 3) ||
         (Number(qtdNiveis) < 2)) {
-        alert("Escreve certinho aí pf"); 
-        
+        alert("Escreve certinho aí pf");
     } 
     else {
-        criarQuizz2();
+        criarQuizz2(); 
     }
 }
 
+/* --------------------------------------------- INICIA TELA 2 DA CRIACAO - - PERGUNTAS */
 let arrayPerguntasAbertas = [];
 let arrayPerguntasFechadas = [];
 
@@ -59,22 +59,22 @@ function criarQuizz2() {
         <div class="perguntaAberta escondido">
             <div class="criacaoConteudo">
             <h2>Pergunta ${i+1}</h2>
-                <div class="box-input textoPergunta"><input type="text" required placeholder="Texto da pergunta"></div> 
+                <div class="box-input textoPergunta"><input type="text" required placeholder="Texto da pergunta (min 20 caracteres)"></div> 
                 <div class="box-input corPergunta"><input type="color" required placeholder="Cor de fundo da pergunta"></div>
 
                 <h2>Resposta correta</h2>
-                <div class="box-input respostaCorreta"><input type="text" required placeholder="Resposta correta"></div>
-                <div class="box-input urlRespostaCorreta"><input type="url" required placeholder="URL da imagem"></div>
+                <div class="box-input respostaCorreta"><input type="text" required placeholder="Resposta correta (obrigatório)"></div>
+                <div class="box-input urlRespostaCorreta"><input type="url" required placeholder="URL da imagem (obrigatório)"></div>
 
                 <h2>Respostas incorretas</h2>
-                <div class="box-input respostaIncorreta1"><input type="text" required placeholder="Resposta incorreta 1"></div>
-                <div class="box-input urlRespostaIncorreta1"><input type="url" required placeholder="URL da imagem 1"></div>
+                <div class="box-input respostaIncorreta1"><input type="text" required placeholder="Resposta incorreta 1 (obrigatório)"></div>
+                <div class="box-input urlRespostaIncorreta1"><input type="url" required placeholder="URL da imagem (obrigatório)"></div>
                 <br>
-                <div class="box-input respostaIncorreta2"><input type="text" placeholder="Resposta incorreta 2"></div>
-                <div class="box-input urlRespostaIncorreta2"><input type="url" pattern="https?://.+"placeholder="URL da imagem 2"></div>
+                <div class="box-input respostaIncorreta2"><input type="text" placeholder="Resposta incorreta 2 (opcional)"></div>
+                <div class="box-input urlRespostaIncorreta2"><input type="url" pattern="https?://.+"placeholder="URL da imagem (se campo acima foi preenchido)"></div>
                 <br>
-                <div class="box-input respostaIncorreta3"><input type="text" placeholder="Resposta incorreta 3"></div>
-                <div class="box-input urlRespostaIncorreta3"><input type="url" placeholder="URL da imagem 3"></div>
+                <div class="box-input respostaIncorreta3"><input type="text" placeholder="Resposta incorreta 3 (opcional)"></div>
+                <div class="box-input urlRespostaIncorreta3"><input type="url" placeholder="URL da imagem (se campo acima foi preenchido)"></div>
             </div>
         </div>`
         let perguntaModeloFechado = `
@@ -155,7 +155,7 @@ function pegarValues2 () {
         
         if ((respostaCorreta.length < 1) || (respostaIncorreta1.length < 1) || (tituloPergunta.length < 20) || 
         (URLrespostaCorreta.substring(0,4) !== "http") || (URLrespostaIncorreta1.substring(0,4) !== "http")) {
-            alert("Em alguma pergunta tem um erro no texto da pergunta - ou na resposta correta - ou na PRIMEIRA resposta incorreta... dá uma conferida aí!");
+            alert("Tem alguma coisa errada. Dá uma conferida aí!");
             arrayPerguntasCriadas = [];
             break;
         } 
@@ -193,7 +193,6 @@ function pegarValues2 () {
                         }
                     ]
                 }
-                criarQuizz3();
             }
         }
 
@@ -224,7 +223,6 @@ function pegarValues2 () {
                         }
                     ]
                 }
-                criarQuizz3()
             }
         }
         else {
@@ -244,56 +242,129 @@ function pegarValues2 () {
                         }
                     ]
                 }
-                criarQuizz3()
         }
 
-        console.log(pergunta);
-        arrayPerguntasCriadas.push(pergunta);
+    arrayPerguntasCriadas.push(pergunta);
+    }
+   
+    if (arrayPerguntasCriadas.length === divsPerguntas.length) {
+        criarQuizz3()
     }
 }
-    
+
+/* --------------------------------- INICIANDO TERCEIRA TELA DA CRIAÇÃO - NIVEIS --------------------------------- */
+let arrayNiveisAbertas = [];
+let arrayNiveisFechadas = [];
 
 function criarQuizz3() {
-    console.log(arrayPerguntasCriadas);
     container.innerHTML = `
     <div class="criacaoQuizz">
-    <div class="criacaoTitulo">
-        <h1>Agora, decida os níveis</h1>
+        <div class="criacaoTitulo">
+            <h1>Agora, decida os níveis</h1>
+        </div>
     </div>
-
-    <div class="criacaoConteudo">
-        <h2>Nível 1</h2>
-        <div class="box-input" ><input placeholder="Título do nível"></div>
-        <div class="box-input"><input placeholder="% de acerto mínima"></div>
-        <div class="box-input"><input placeholder="URL da imagem do nível"></div>
-        <div class="box-input"><input placeholder="Descrição do nível"></div>
-    </div>
-    <div class="criacaoConteudoMini">
-        <h2>Nível 3</h2>
-        <img src="img/editar.svg" alt="">
-    </div>
-
-    <div class="botaoVermelho370" onclick="EnviarQuizz()">Finalizar Quizz</div>
-</div> 
     `
+        for (let i=0; i<qtdNiveis; i++) {
+            
+        let nivelModelo = `
+        <div class="nivelAberta escondido">
+            <div class="criacaoConteudo">
+                <h2>Nivel ${i+1}</h2>
+                <div class="box-input tituloNivel"><input type="text" placeholder="Título do nível (min 10 caracteres)"></div>
+                <div class="box-input porcentagemAcerto"><input type="number" placeholder="% de acerto mínima (numero de 0 a 100)"></div>
+                <div class="box-input imagemNivel"><input type="url" placeholder="URL da imagem do nível"></div>
+                <div class="box-input descricaoNivel"><input type="text" placeholder="Descrição do nível (min 30 caracteres)"></div>
+            </div>
+        </div>`
+        let nivelModeloFechado = `
+        <div class="nivelFechada">
+            <div class="criacaoConteudoMini">
+                <h2>Nivel ${i+1}</h2>
+                <img src="img/editar.svg" onclick="editarnivel(this)">
+            </div>
+        </div>`
+
+        arrayNiveisAbertas.push(`${nivelModelo}`);
+        arrayNiveisFechadas.push(`${nivelModeloFechado}`);
+        
+        container.innerHTML += arrayNiveisAbertas[i];
+        container.innerHTML += arrayNiveisFechadas[i];
+        }
+
+        document.querySelector(".nivelAberta").classList.remove("escondido");
+        document.querySelector(".nivelFechada").classList.add("escondido");
+
+        container.innerHTML += `<div class="botaoVermelho370" onclick="pegarValues3()">Finalizar Quizz</div>`;
 }
 
-function criarQuizz4() {
-    container.innerHTML = `
-    <div class="criacaoQuizz">
-    <div class="criacaoTitulo">
-        <h1>Seu quizz está pronto!</h1>
-    </div>
+function editarnivel(elemento) {
+    let nivelAbertas = document.querySelectorAll(".nivelAberta");
+
+    for (let i=0; i<nivelAbertas.length; i++) {
+        nivelAbertas[i].classList.add("escondido");
+    }
+        
+
+    let nivelFechadas = container.querySelectorAll(".nivelFechada");
+    for (let i=0; i<nivelFechadas.length; i++) {
+        nivelFechadas[i].classList.remove("escondido");
+    }
 
    
-    <div class="botaoVermelho221" onclick="acessarQuizz()">Acessar Quizz</div>
-    <div class="botaoVermelho221" onclick="voltarprahome()">Voltar pra home</div>
-</div>
-    `
+
+    let elementoPai = elemento.parentNode;
+    let elementoAvo = elementoPai.parentNode;
+
+
+    let elementoAnterior = elementoAvo.previousElementSibling;
+
+    elementoAvo.classList.add("escondido");
+
+    elementoAnterior.classList.remove("escondido");
+
 }
 
+let arrayNiveisCriados = [];
 
+function pegarValues3 () {
+   
+    let divsNiveis = container.querySelectorAll(".criacaoConteudo");
+    let nivel = {};
+    let verificaPorcentagem = 0;
+    for (let i=0; i<divsNiveis.length; i++) {
+        
+        let tituloNivel = divsNiveis[i].querySelector(".tituloNivel input").value;
+        let porcentagemAcertoString = divsNiveis[i].querySelector(".porcentagemAcerto input").value;
+            let porcentagemAcerto = Number(porcentagemAcertoString);
+        let imagemNivel = divsNiveis[i].querySelector(".imagemNivel input").value;
+        let descricaoNivel = divsNiveis[i].querySelector(".descricaoNivel input").value;
 
+        if ((imagemNivel.substring(0,4) !== "http") || (tituloNivel.length<10) || (descricaoNivel.length<30) || (porcentagemAcerto<0 || porcentagemAcerto>100)) {
+            alert("Confere tudinho aí porque alguma coisa está errada!");
+            arrayNiveisCriados = [];
+            break
+        } else {
+            nivel = {
+                title: tituloNivel,
+                image: imagemNivel,
+                text: descricaoNivel,
+                minValue: porcentagemAcerto
+            }
+        }
+        if (porcentagemAcerto === 0) {
+            verificaPorcentagem = 1;
+        }
+        
+        arrayNiveisCriados.push(nivel);
+    }
+
+    if ((arrayNiveisCriados.length === divsNiveis.length) && (verificaPorcentagem === 0)) {
+        alert("É obrigatório que um dos níveis tenha a % mínima com o valor 0");
+        arrayNiveisCriados = [];
+    } else if ((arrayNiveisCriados.length === divsNiveis.length) && (verificaPorcentagem === 1)) {
+       EnviarQuizz()
+    }
+}
 
 
 function EnviarQuizz() {
@@ -303,25 +374,12 @@ function EnviarQuizz() {
         image: imgQuizz,
         questions: arrayPerguntasCriadas,
 
-        levels: [
-            {
-                title: "Título do nível 1",
-                image: "https://http.cat/411.jpg",
-                text: "Descrição do nível 1",
-                minValue: 0
-            },
-            {
-                title: "Título do nível 2",
-                image: "https://http.cat/412.jpg",
-                text: "Descrição do nível 2",
-                minValue: 50
-            }
-        ]
+        levels: arrayNiveisCriados
     }
-        console.log(criandoQuizz);
 
 
-    const promise = axios.post("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes", criandoQuizz);
+
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes", criandoQuizz); 
     promise.then(quizzEnviado);
     promise.catch(erroCriacaoQuizz);
 }
@@ -333,6 +391,26 @@ function quizzEnviado() {
  function erroCriacaoQuizz(erro) {
     console.log("Deu ruim");
  }
+
+ 
+
+ function criarQuizz4() {
+    container.innerHTML = `
+    <div class="criacaoQuizz">
+    <div class="criacaoTitulo">
+        <h1>Seu quizz está pronto!</h1>
+    </div>
+
+    <div class="criacaoConteudoFinal">
+        <img src=${imgQuizz} alt="">
+        <div class="gradiente"></div>
+        <h1>${tituloQuizz}</h1>
+    </div>
+    <div class="botaoVermelho221" onclick="acessarQuizz()">Acessar Quizz</div>
+    <div class="botaoVermelho221Branco" onclick="voltarprahome()">Voltar pra home</div>
+</div>
+    `
+}
 
 function voltarprahome() {
     window.location.reload();
